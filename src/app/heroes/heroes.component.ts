f43b2de0d1ae26accdb6f12a0842dee1ace6e3f6
s-tamiya <sy.tamiya@gmail.com>
2018-07-20 12:24:58 +0900

    dashboard追加

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -8,27 +9,16 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
-
-    heroes: Hero[];
-    selectedHero: Hero;
+  heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
-      this.getHeroes();
-  }
-
-  onSelect(hero: Hero) : void {
-      this.selectedHero = hero;
+    this.getHeroes();
   }
 
   getHeroes(): void {
-      this.heroService
-            .getHeroes()
-            .subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
